ab794b62181ca59cd5b819d42abf60fd6bff22c0
adnan18py <ahmadkazemi29adnan7@gmail.com>
2026-09-15 14:13:59 +0330

    Hmmm

--- plugins/violation_manager/plugin.py
+++ plugins/violation_manager/plugin.py
@@ -1,17 +1,18 @@
 from . import handlers
+from .store import ViolationStore
 from core.base_plugin import BasePlugin
 
 
 class ViolationManagerPlugin(BasePlugin):
 
     name = "Violation Manager"
-    version = "0.8.0"
+    version = "0.9.0"
 
     def __init__(self, client, command_manager, db):
         super().__init__(client, command_manager, db)
-
+        self.violations = ViolationStore(self.db)
     async def on_load(self):
-        pass
+        await self.violations.create_table()
 
     async def on_enable(self):
         pass
