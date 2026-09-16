d9b3a7c9950d8b81f9a2b5d25fddfa19903864c5
adnan18py <ahmadkazemi29adnan7@gmail.com>
2026-09-16 17:53:50 +0330

    هورا😒

--- plugins/system_plugin/backup/backup.py
+++ plugins/system_plugin/backup/backup.py
@@ -4,7 +4,7 @@ import tempfile
 
 from config import config
 from core.database_manager import DatabaseManager
-from github_manager.manager import GitHubManager
+from ..github_manager.manager import GitHubManager
 
 
 class DatabaseBackupManager:
